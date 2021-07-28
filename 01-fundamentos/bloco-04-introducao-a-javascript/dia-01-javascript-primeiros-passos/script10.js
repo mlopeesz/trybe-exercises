33cf5896a4e56c5f5f48163e023ddbbb1126ef43
Mauricio Lopes <mlopeesz@gmail.com>
2021-07-28 19:16:11 -0300

    Realizado exercicio 10

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/script10.js
@@ -0,0 +1,8 @@
+let valorCusto = 500;
+let valorVenda = 1000;
+
+let valorCustoTotal = valorCusto + (valorCusto * 0.2);
+let lucro = valorVenda - valorCustoTotal;
+
+let lucroTotal = lucro * 1000;
+console.log("O lucro ao vender 1000 produtos é:", lucroTotal)
