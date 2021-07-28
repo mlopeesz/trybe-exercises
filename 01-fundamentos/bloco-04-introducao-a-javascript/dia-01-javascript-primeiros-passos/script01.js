12144b07d1852f3093c9847de7eab1b6cd9d71b1
Mauricio Lopes <mlopeesz@gmail.com>
2021-07-28 18:03:42 -0300

    Realizado exercicio 1

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/script01.js
@@ -0,0 +1,17 @@
+let num1 = 10;
+let num2 = 5;
+
+//Soma
+let sum = num1 + num2;
+
+//Subtração
+let sub = num1 - num2;
+
+//Multiplicação
+let mult = num1 * num2;
+
+//Divisão
+let div = num1 / num2;
+
+//Módulo
+let mod = num1 % num2;
