e4f5b541e4a44577c0f3af3e97a746c38ee928ae
Mauricio Lopes <mlopeesz@gmail.com>
2021-07-28 18:18:50 -0300

    Realizado exercicio 3

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/script02.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/script02.js
@@ -1,5 +1,5 @@
-let num1 = 15
-let num2 = 10
+let num1 = 15;
+let num2 = 10;
 
 if (num1 > num2) {
   console.log(num1)
---
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/script03.js
@@ -0,0 +1,15 @@
+let num1 = 30;
+let num2 = 50;
+let num3 = 20;
+
+if (num1 > num2 && num1 > num3) {
+  console.log(num1)
+}
+
+else if (num2 > num1 && num2 > num3) {
+  console.log(num2)
+}
+
+else if (num3 > num1 && num3 > num2) {
+  console.log(num3)
+}
