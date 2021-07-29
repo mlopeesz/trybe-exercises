e9b8a71a4a4ec8508213e9f485f1f028323075a7
Mauricio Lopes <mlopeesz@gmail.com>
2021-07-29 18:39:21 -0300

    Realizado exercicio 6

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-02-array-e-loop-for/exercicio06.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-02-array-e-loop-for/exercicio06.js
@@ -1,1 +1,18 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+let contadorImpar = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+  if ((numbers[index] % 2) === 0) {
+    contadorImpar += 0;
+  }
+  else {
+    contadorImpar += 1;
+  }
+}
+if (contadorImpar === 0) {
+  console.log('Nenhum valor impar encontrado');
+}
+else {
+  console.log('Foram encontrados', contadorImpar, 'números ímpares.');
+}
