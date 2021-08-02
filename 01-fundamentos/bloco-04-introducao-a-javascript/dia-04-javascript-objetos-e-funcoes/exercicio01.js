0ee1748e3a8efa25beeee177966b63a23e244007
Mauricio Lopes <mlopeesz@gmail.com>
2021-08-02 18:49:03 -0300

    Realizado exercicio 1.2

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicio01.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicio01.js
@@ -4,4 +4,9 @@ let info = {
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
+//1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
 console.log('Bem vinda ' + info.personagem)
+
+//2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+info.recorrente = 'Sim';
+console.log(info)
