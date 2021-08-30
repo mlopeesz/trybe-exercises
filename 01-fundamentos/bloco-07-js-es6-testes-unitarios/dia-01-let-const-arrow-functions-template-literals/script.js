132ff2db55cb73ae4fd3e7cf5245ee8178430b28
Mauricio <mlopeesz@gmail.com>
2021-08-30 12:31:32 -0300

    Realizado exercicios - parte 1

--- 01-fundamentos/bloco-07-js-es6-testes-unitarios/dia-01-let-const-arrow-functions-template-literals/script.js
+++ 01-fundamentos/bloco-07-js-es6-testes-unitarios/dia-01-let-const-arrow-functions-template-literals/script.js
@@ -1,13 +1,23 @@
-function testingScope(escopo) {
+const testingScope = (escopo) => {
     if (escopo === true) {
-      var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
       console.log(ifScope);
     } else {
-      var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
       console.log(elseScope);
     }
-    console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
   }
 
   testingScope(true);
+
+  // Faça uma função que retorne o array oddsAndEvens em ordem crescente.
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+  const sortArray = (array) => {
+      const sortOddsAndEvens = array.sort((a,b) => a-b);
+      return sortOddsAndEvens
+  }
+
+  console.log(`Os números ${sortArray(oddsAndEvens)} se encontram ordenados de forma crescente!`);
