a37c2beadfc5b554935ad0861904269ca47a5516
Mauricio <mlopeesz@gmail.com>
2021-09-08 17:44:04 -0300

    Realizado exercicios

--- 01-fundamentos/bloco-08-hofs-js/dia-02-forEach-find-some-every-sort/exercise3.js
+++ 01-fundamentos/bloco-08-hofs-js/dia-02-forEach-find-some-every-sort/exercise3.js
@@ -64,3 +64,20 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  // escreva seu código aqui
+  
+}
+
+assert.deepStrictEqual(getNamedBook(), expectedResult);
