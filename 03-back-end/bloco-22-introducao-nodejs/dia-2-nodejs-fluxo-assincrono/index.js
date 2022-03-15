342151efb302ecc5b0a8304b7fda105111e7c065
Mauricio Lopes <mlopeesz@gmail.com>
2022-03-15 12:07:34 -0300

    Exercicio 2

--- 03-back-end/bloco-22-introducao-nodejs/dia-2-nodejs-fluxo-assincrono/index.js
+++ 03-back-end/bloco-22-introducao-nodejs/dia-2-nodejs-fluxo-assincrono/index.js
@@ -1,4 +1,5 @@
-/* Crie uma função que recebe três parâmetros retorna uma Promise .
+/* Exercício 1
+Crie uma função que recebe três parâmetros retorna uma Promise .
 Caso algum dos parâmetros recebidos não seja um número, rejeite a Promise com o motivo "Informe apenas números" .
 Caso todos os parâmetros sejam numéricos, some os dois primeiros e multiplique o resultado pelo terceiro ( (a + b) * c ).
 Caso o resultado seja menor que 50, rejeite a Promise com o motivo "Valor muito baixo"
@@ -26,3 +27,21 @@ matemagica(1, 1, 1)
 matemagica(10, 10, 10)
   .then(resolve => console.log(resolve))
   .catch(error => console.log(error))
+
+
+/* Exercício 2
+Escreva um código para consumir a função construída no exercício anterior.
+Gere um número aleatório de 1 a 100 para cada parâmetro que a função recebe. Para gerar um número aleatório, utilize o seguinte trecho de código: Math.floor(Math.random() * 100 + 1) .
+Chame a função do exercício anterior, passando os três números aleatórios como parâmetros.
+Utilize then e catch para manipular a Promise retornada pela função:
+Caso a Promise seja rejeitada, escreva na tela o motivo da rejeição.
+Caso a Promise seja resolvida, escreva na tela o resultado do cálculo. */
+
+const generateRandomNumber = () => Math.floor(Math.random() * 100 + 1);
+
+const randomNumbers = Array.from({ length: 3 }).map(generateRandomNumber);
+
+
+matemagica(...randomNumbers)
+  .then(result => console.log(result))
+  .catch(error => console.log(error.message))
